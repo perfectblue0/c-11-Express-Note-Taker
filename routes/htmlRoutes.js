bcfdb82014178b623ac9b28db6b7911ec6ee6b13
Paola_G <perfectblue@live.com>
2023-06-02 18:17:07 -0700

    added file names to gitignore, started server.js code, created routes files, added code to htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// GET route to index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// GET route to notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
